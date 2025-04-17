e346632c34f3e2f6a7aa9adfc398331edf0972d0
sagarkumarnri <patrsagarkumkar88@gmail.com>
2025-04-17 15:26:43 +0530

    calculator

--- src/main/java/com/benificiary/namecheck/calculatorff4/config/FF4JConfig.java
+++ src/main/java/com/benificiary/namecheck/calculatorff4/config/FF4JConfig.java
@@ -1,23 +1,36 @@
 package com.benificiary.namecheck.calculatorff4.config;
 
-import org.springframework.context.annotation.Bean;
+
+
 
 import org.ff4j.FF4j;
+import org.ff4j.web.FF4jDispatcherServlet;
 import org.ff4j.core.Feature;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FF4JConfig {
+
     @Bean
     public FF4j getFF4j() {
         FF4j ff4j = new FF4j();
 
-        // Define features
         ff4j.createFeature(new Feature("feature-add", true));
         ff4j.createFeature(new Feature("feature-subtract", true));
-        ff4j.createFeature(new Feature("feature-divide", false)); // disabled by default
+        ff4j.createFeature(new Feature("feature-divide", false));
 
         return ff4j;
+    }
+
+    @Bean
+    public ServletRegistrationBean<FF4jDispatcherServlet> ff4jServlet(FF4j ff4j) {
+        FF4jDispatcherServlet ff4jServlet = new FF4jDispatcherServlet();
+        ff4jServlet.setFf4j(ff4j);
+        ServletRegistrationBean<FF4jDispatcherServlet> registration =
+                new ServletRegistrationBean<>(ff4jServlet, "/ff4j-console/*");
+        registration.setName("FF4jConsoleServlet");
+        return registration;
     }
 }
